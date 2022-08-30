bba2c75845abb0f3e63739453e23acbdff781529
andynet <andrejbalaz001@gmail.com>
2022-08-30 16:52:40 +0200

    added clean_phages.sh

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # shellscripts
+
+this repository is for small shellscripts outside of any project, which are still worthy of backing up
---
+++ clean_phages.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# inputs:
+# sequences.fasta -> original.fna
+
+# needed software:
+# seqkit, taxonkit
+
+set -euxo pipefail
+
+export TAXONKIT_DB=$(realpath ./taxonkit_db)
+
+seqkit fx2tab original.fna                              \
+    | grep "complete genome"                            \
+    | grep -v "UNVERIFIED"                              \
+    | sed -e "s/|/\t/g"                                 \
+    | grep -Pv "\t\t"                                   \
+    | taxonkit name2taxid -i 3                          \
+    | taxonkit lineage -R -i 6                          \
+    | awk -F "\t" '{ print $1 " " $7 " " $8 "\t" $4}'   \
+    | seqkit tab2fx -w 0                                \
+    | grep "Bacteria" -A 1 --no-group-separator         \
+    > clean.fna
+
+# ktImportText -q hosts_krona.txt
+
