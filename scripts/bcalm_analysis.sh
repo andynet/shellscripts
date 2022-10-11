0c83bfbb0924207d6464c1f7ed9205c0e4e4fec2
andynet <andrejbalaz001@gmail.com>
2022-10-11 17:52:39 +0200

    added bcalm analysis

---
+++ scripts/bcalm_analysis.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+[[ $- != *i* ]] && set -euxo pipefail
+
+export PROJ_DIR="/home/andy/projects/phages"
+
+function extract_cluster() {
+    file=$1;
+    clust_num=$2;
+    fasta=$3;
+    output=$4;
+
+    grep -P "\t${clust_num}$" "${file}" | cut -f 1 > tmp
+    grep -A 1 -f tmp --no-group-separator "${fasta}" > "${output}"
+    rm tmp
+}
+
+function get_unitigs() {
+    input=$1;
+    k=$2;
+    output=$3;
+
+    bcalm -in "${input}" -kmer-size "${k}" -abundance-min 0 -out tmp > /dev/null 2>&1
+    # bcalm -in "${input}" -kmer-size "${k}" -abundance-min 0 -out tmp
+    "${PROJ_DIR}/tools/bcalm/scripts/convertToGFA.py" tmp.unitigs.fa "${output}" "${k}" > /dev/null 2>&1
+    # "${PROJ_DIR}/tools/bcalm/scripts/convertToGFA.py" tmp.unitigs.fa "${output}" 100
+    rm tmp.unitigs.fa
+}
+
+# extract_cluster clusters 4 seqs.fna cluster4.fna
+# get_unitigs cluster4.fna 190 cluster4.gfa
