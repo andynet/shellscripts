0fb72bb1c47f24c747dee90d252e503d5f1ae1a1
andynet <andrejbalaz001@gmail.com>
2022-11-14 14:23:09 +0100

    refactored supported phages finding

--- scripts/bandage_data.sh
+++ scripts/bandage_data.sh
@@ -1,30 +1,52 @@
 #!/bin/bash
-set -euxo pipefail
+[[ $- != *i* ]] && set -euxo pipefail
 
-DB=${1%.fna}            # .fna
-ASSEMBLY=${2%.gfa}      # .gfa
-MIN_SIZE=1000
+# create_blastdb phages.fna -> phages_db
+function create_blastdb() {
+    sequences=${1}
 
-if [[ ! -e ${DB}.2line.fna.ndb ]]; then
-    seqkit seq -w 0 ${DB}.fna > ${DB}.2line.fna
-    makeblastdb -dbtype nucl -in ${DB}.2line.fna
-fi
+    base="$(basename "${sequences%.*}")"
+    index_dir="$(dirname "${sequences}")/${base}_db"
+    echo "Index stored to ${index_dir}"
 
-gfatools gfa2fa ${ASSEMBLY}.gfa > ${ASSEMBLY}.fna
-seqkit seq -m ${MIN_SIZE} ${ASSEMBLY}.fna > ${ASSEMBLY}.large.fna
+    mkdir -p "${index_dir}"
+    seqkit seq -w 0 "${sequences}" > "${index_dir}/2line.fna"
 
-blastn -max_target_seqs 1 -max_hsps 1 -outfmt 6     \
-    -query ${ASSEMBLY}.large.fna                    \
-    -db ${DB}.2line.fna > ${ASSEMBLY}.blast
+    makeblastdb                                 \
+        -dbtype nucl                            \
+        -in "${index_dir}/2line.fna"    \
+        -out "${index_dir}/idx"
+}
 
-less ${ASSEMBLY}.blast | cut -f 2 | sort | uniq -c | sort -rn | nl \
-    | tr -d "\t" | tr -s " " | tr " " "\t" | cut -f 4 > ${ASSEMBLY}.supported.txt
+# get_supported_phages phages_db assembly.gfa -> assembly_phages.fna
+function get_supported_phages() {
+    blast_db=${1}   # blast_db
+    assembly=${2}   # assembly.gfa
+    min_size="1000"
 
-grep --no-group-separator -A 1 -f ${ASSEMBLY}.supported.txt ${DB}.2line.fna \
-    > ${ASSEMBLY}.supported.fna
+    name="${assembly%%.*}"
+
+    gfatools gfa2fa "${assembly}" > "${name}.fna"
+
+    seqkit seq -m "${min_size}" "${name}.fna" > "${name}.large.fna"
+
+    blastn -max_target_seqs 1 -max_hsps 1 -outfmt 6     \
+        -query "${name}.large.fna"                      \
+        -db "${blast_db}/idx" > "${name}.blast"
+
+    less "${name}.blast" | cut -f 2 | sort | uniq -c | sort -rn | nl    \
+        | tr -d "\t" | tr -s " " | tr " " "\t" | cut -f 4               \
+        > "${name}.supported.txt"
+
+    grep --no-group-separator -A 1 -f "${name}.supported.txt" "${blast_db}/2line.fna" \
+        > "${name}.supported.fna"
+
+    rm "${name}".{fna,large.fna,blast,supported.txt}
+}
+
+# create_blastdb phages.fna -> phages_db
+# get_supported_phages phages_db assembly.gfa -> assembly_phages.fna
 
 # cd-hit-est -T 6 -c 0.9 -i ${ASSEMBLY}.supported.fna -o ${ASSEMBLY}.relatives.fna
-
-rm ${ASSEMBLY}.{large.fna,blast,supported.txt}
 # gfatools view -l 1132015 -r 500 assembly_graph.gfa > subgraph.gfa
 # seqkit sort -l phages_2022-07-22.2line.fna | seqkit seq -w 0 > phages_2022-07-22.2line.rsorted.fna
