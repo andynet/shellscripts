c899895202ad4e1c78fda4211bb3aa56509ced09
andynet <andrejbalaz001@gmail.com>
2023-01-23 16:17:37 +0100

    budded filter

--- scripts/fluente_server.sh
+++ scripts/fluente_server.sh
@@ -2,19 +2,22 @@
 # conda activate fluente
 set -euo pipefail
 
-ROOT_DIR="/data/projects/fluente"
-
 function make_idx() {
-    makeblastdb                                                                 \
-        -dbtype nucl                                                            \
-        -in  "${ROOT_DIR}/reference/rvp/rvp.fa"                                 \
-        -out "${ROOT_DIR}/reference/rvp/idx"
+    fasta="${1}"
+    dirname=$(dirname $(realpath "${fasta}"))
 
-    mkdir -p "${ROOT_DIR}/reference/rvp/drafts"
-    fastaexplode -f "${ROOT_DIR}/reference/rvp/rvp.fa"                          \
-        -d "${ROOT_DIR}/reference/rvp/drafts"
+    makeblastdb                 \
+        -dbtype nucl            \
+        -in  "${fasta}"         \
+        -out "${dirname}/idx"
+
+    mkdir -p "${dirname}/drafts"
+    sed "s/\//_/g;s/|/_/g" "${fasta}" > "${fasta}.tmp"
+    fastaexplode                \
+        -f "${fasta}.tmp"       \
+        -d "${dirname}/drafts"
+    rm "${fasta}.tmp"
 }
-export -f make_idx
 
 function assemble() {
     SAMPLE=${1}
@@ -27,7 +30,6 @@ function assemble() {
             -o ${ROOT_DIR}/assembly_branch/${SAMPLE}/spades
     "
 }
-export -f assemble
 
 function scaffold() {
     SAMPLE=${1}
@@ -37,38 +39,64 @@ function scaffold() {
         -i "${ROOT_DIR}/assembly_branch/${SAMPLE}/spades/contigs.fasta"         \
         -f "${ROOT_DIR}/reference/rvp/drafts"                                   \
         -o "${ROOT_DIR}/assembly_branch/${SAMPLE}/medusa/scaffolds.fa"
+}
 
-    blastn -outfmt 6                                                            \
-        -max_target_seqs 1                                                      \
-        -max_hsps 1                                                             \
-        -perc_identity 90                                                       \
-        -query "${ROOT_DIR}/assembly_branch/${SAMPLE}/medusa/scaffolds.fa"      \
-        -db "${ROOT_DIR}/reference/rvp/idx"                                     \
-        | awk '$4 > 500 {print $0}' | cut -f1 | sed "s/$/\$/"                   \
-        > "${ROOT_DIR}/assembly_branch/${SAMPLE}/medusa/rvp_scaffolds.txt"
+function filter() {
+    query="${1}"
+    reference="${2}"
+
+    output="$(dirname $(realpath ${query}))"
+    database="$(dirname $(realpath ${reference}))/idx"
+
+    blastn -outfmt 6                \
+        -max_target_seqs 1          \
+        -max_hsps 1                 \
+        -perc_identity 90           \
+        -query "${query}"           \
+        -db "${database}"           \
+        | awk '$4 > 500 {print $0}' \
+        | cut -f1                   \
+        | sed "s/$/\$/"             \
+        > "${output}/filtrate.txt"
 
     grep --no-group-separator -A 1                                              \
-        -f "${ROOT_DIR}/assembly_branch/${SAMPLE}/medusa/rvp_scaffolds.txt"     \
-        "${ROOT_DIR}/assembly_branch/${SAMPLE}/medusa/scaffolds.fa"             \
-        >  "${ROOT_DIR}/assembly_branch/${SAMPLE}/medusa/rvp_scaffolds.fa"
+        -f "${output}/filtrate.txt"    \
+        "${query}" \
+        > "${output}/filtrate.fa"
 }
+
+export -f make_idx
+export -f assemble
 export -f scaffold
+export -f filter
 
-for file in ../../reads/deduplicated/run*_R1.fastq.gz; do
-    f=$(basename "${file%_R1*}");
+# ROOT_DIR="/data/projects/fluente"
+# ROOT_DIR="/home/andy/projects/phages/data/fluente"
 
-    if [[ ! -e "${ROOT_DIR}/assembly_branch/${f}/spades/contigs.fasta" ]];
-    then
-        echo "Processing ${f}";
-        assemble "${f}";
-    fi
-done;
+# make_idx "${ROOT_DIR}/reference/rvp/rvp.fa"
+# make_idx "${ROOT_DIR}/reference/h3n2/h3n2_ha_gene_2021.fa"
 
-while [[ $(qstat | wc -l ) -ne 0 ]]; do sleep 30; done;
+# for file in ../../reads/deduplicated/run*_R1.fastq.gz; do
+#     f=$(basename "${file%_R1*}");
 
-for file in ${ROOT_DIR}/assembly_branch/*/spades/contigs.fasta; do
-    f=$(basename $(dirname $(dirname ${file})));
-    echo "Processing ${f}";
-    scaffold ${f} > /dev/null;
-done;
+#     if [[ ! -e "${ROOT_DIR}/assembly_branch/${f}/spades/contigs.fasta" ]];
+#     then
+#         echo "Processing ${f}";
+#         assemble "${f}";
+#     fi
+# done;
 
+# while [[ $(qstat | wc -l ) -ne 0 ]]; do sleep 30; done;
+
+# for file in ${ROOT_DIR}/assembly_branch/*/spades/contigs.fasta; do
+#     f=$(basename $(dirname $(dirname ${file})));
+#     echo "Processing ${f}";
+#     scaffold ${f} > /dev/null;
+# done;
+
+# reference="${ROOT_DIR}/reference/h3n2/h3n2_ha_gene_2021.fa"
+
+# for file in ${ROOT_DIR}/assembly_branch/*/medusa/scaffolds.fa; do
+#     echo ${file};
+#     filter "${file}" "${reference}"
+# done;
